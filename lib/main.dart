975f946910d2f7f447abb8e20a55c3d01bfbffa8
Chidambararajab <chidambararaja97@gmail.com>
2021-07-25 23:08:37 +0530

    Commiting | StatefullWidget Basic Setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex++;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(title: Text('Hello World'),),
-      body: Text('This is my default text!'),
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite foods?',
+      'What\'s your favorite place?'
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Column(
+        children: [
+          Text(
+            questions[questionIndex],
+          ),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
     ));
   }
 }
